0116e48f261cfe7984aabf4827a758b8deb0b986
MWill1 <mwill1017@gmail.com>
2016-09-22 14:49:40 -0400

    Updated Char Select Screen
    Added Fight! button and some functionality

---
+++ Kinetic_Beginnings/Assets/Scripts/CharSelectionScreen/GoToFightButton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GoToFightButton : MonoBehaviour {
+
+    public Button thisButton;
+    public Image[] partyImages = new Image[3];
+    PartySpot spot;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PartyCheck();
+	}
+
+    void PartyCheck()
+    {
+        foreach (Image pI in partyImages)
+        {
+            spot = pI.GetComponent<PartySpot>();
+            if (spot.CheckIsFilled() == true)
+            {
+                Debug.Log("IsFilled is true for " + pI.name);
+                thisButton.interactable = true;
+            }
+
+            else
+            {
+                return;
+            }
+        }
+    }
+}
