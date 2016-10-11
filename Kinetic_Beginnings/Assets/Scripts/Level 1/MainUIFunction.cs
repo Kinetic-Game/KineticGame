f1ac40324b77c34826fbd10fedc7a0c9d8efa72a
laz89 <laz89@msn.com>
2016-10-10 23:47:56 -0400

    Function to UI
    Added some functionallity to the UI. Got the animations in place and
    fooled around with the camera a bit. Added the animations in the
    animations folder>Test. Also organized my scripts as well.

---
+++ Kinetic_Beginnings/Assets/Scripts/Level 1/MainUIFunction.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainUIFunction : MonoBehaviour {
+
+	//public bool whenPressed;
+
+	public GameObject camera;
+
+	public GameObject cameraOne;
+
+	public Animator attackTest;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void DisableCamera1()
+	{
+		cameraOne.SetActive (false);
+	}
+
+	public void OnPressAttack()
+	{
+		print ("Pow!");
+
+		//whenPressed = true;
+
+		//attackTest.SetBool ("whenPressed", true);
+
+		attackTest.Play ("AttackTest");
+
+		cameraOne.SetActive (true);
+
+		Invoke("DisableCamera1", 2.0f);
+	}
+
+	public void OnPressDefend()
+	{
+		print ("Protect!");
+	}
+
+	public void OnPressKinesis()
+	{
+		print ("Power!");
+	}
+
+	public void OnPressRest()
+	{
+		print ("Pant!");
+	}
+
+	public void OnPressHealth()
+	{
+		print ("Phew!");
+	}
+
+	public void OnPressEnergy()
+	{
+		print ("Potion!");
+	}
+}
