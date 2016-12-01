f21f79f19e584e25030fb9fe14740305081733b0
laz89 <laz89@msn.com>
2016-12-01 07:33:21 -0500

    minor changes
    Added scripts for stat system. Added animations.

---
+++ Kinetic_Beginnings/Assets/Resources/Scripts/BasicCharStats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicCharStats : MonoBehaviour {
+
+	public static float curHealth;
+
+	public static float maxHealth = 100;
+
+	public static float curKinesis;
+
+	public static float maxKinesis = 100;
+
+	public static float attack = 25;
+
+	public static float defense = 10;
+
+	public static float rest = 10;
+
+	public static float levelMultiplier = 1.0f;
+
+	public static int level = 1;
+
+
+}
