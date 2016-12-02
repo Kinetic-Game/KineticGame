fef796d8c12f1c7772e8ae62da99ee87712fdc66
MWill1 <mwill1017@gmail.com>
2016-12-01 20:55:02 -0500

    Beginning of combat system
    Beginning scripts of combat system

---
+++ Kinetic_Beginnings/Assets/Resources/Scripts/AlexAttributes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlexAttributes : CharAttributes {
+
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        curHealth = maxHealth;
+        curKinetic = maxKinetic;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+}
---
+++ Kinetic_Beginnings/Assets/Resources/Scripts/CharAttributes.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+
+public class CharAttributes {
+
+    public static float curHealth;
+    public static float maxHealth;
+    public static float curKinetic;
+    public static float maxKinetic;
+    public static float restHamt;
+    public static float restKamt;
+    public static float defense;
+    public static int curLevel;
+    public static int lvlIncAmt;
+    public static int curXP;
+
+
+    public static void AdjHealth(float _amt)
+    {
+        if (_amt < 0)
+        {
+            float temp = _amt + defense;
+            if (temp >= 0)
+            {
+                return;
+            }
+            else
+            {
+                curHealth += temp;
+            }
+        }
+        else
+        {
+            curHealth += _amt;
+        }
+        
+    }
+
+    public static void Rest()
+    {
+        curHealth += restHamt;
+        curKinetic += restKamt;
+    }
+
+    public static void LevelUp()
+    {
+        curLevel++;
+    }
+
+    public static void AddXP(int _amt)
+    {
+        curXP += _amt;
+
+        if (curXP >= curLevel * lvlIncAmt)
+        {
+            LevelUp();
+        }
+    }
+}
