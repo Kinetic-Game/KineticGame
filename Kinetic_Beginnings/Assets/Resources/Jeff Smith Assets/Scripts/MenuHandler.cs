7cf13136ec07841c083b1e057435b9635b700f04
Jefferius <smithjeffrey2@ymail.com>
2016-12-03 15:00:26 -0800

    Updated Main Menu
    Added Options menu and credits page.

---
+++ Kinetic_Beginnings/Assets/Resources/Jeff Smith Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour {
+
+    public GameObject MainMenu;
+    public GameObject OptionsMenu;
+    public GameObject CreditsMenu;
+
+    // Use this for initialization
+    void Start () {
+        OptionsMenu.SetActive(false);
+        CreditsMenu.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OptionsButtonOpen ()
+    {
+        MainMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+
+    }
+
+    public void OptionsButtonClose()
+    {
+        MainMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+    }
+
+    public void CreditsButtonOpen()
+    {
+        OptionsMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+
+    }
+
+    public void CreditsButtonClose()
+    {
+        OptionsMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+
+
+
+}
